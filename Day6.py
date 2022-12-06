1bdf196b73641c39ffa76797f27ad177ba3b2432
Dinesh G <49871841+idineshgovind@users.noreply.github.com>
2022-12-06 12:28:26 +0530

    Reformatted the code

--- Day6.py
+++ Day6.py
@@ -3,16 +3,18 @@ signal = day6.read().rstrip()
 
 signal_length = len(signal)
 
+
 def p1(chars):
-  i = 0
+    i = 0
 
-  while i < signal_length:
-    string = signal[i:i+chars]
+    while i < signal_length:
+        string = signal[i:i + chars]
 
-    if len(set(string)) == chars:
-      return i + chars
+        if len(set(string)) == chars:
+            return i + chars
 
-    i += 1
+        i += 1
 
-print("Part 1 :",str(p1(4)))
-print("Part 2 :",str(p1(14)))
+
+print("Part 1 :", str(p1(4)))
+print("Part 2 :", str(p1(14)))
