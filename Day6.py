0032304872d237611c7884622b81eac83a8e9aff
Dinesh G <49871841+idineshgovind@users.noreply.github.com>
2022-12-06 11:15:53 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,18 @@
+day6 = open("input.txt", 'r')
+signal = day6.read().rstrip()
+
+signal_length = len(signal)
+
+def p1(chars):
+  i = 0
+
+  while i < signal_length:
+    string = signal[i:i+chars]
+
+    if len(set(string)) == chars:
+      return i + chars
+
+    i += 1
+
+print("Part 1 :",str(p1(4)))
+print("Part 2 :",str(p1(14)))
